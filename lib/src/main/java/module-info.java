fc20b1fb7341b32e75ceac1b82b0b9d9b88079b3
intrigus <abc123zeus@live.de>
2022-09-07 23:33:54 +0200

    Move files from "src" to "lib/src".

---
+++ lib/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.brunomnsilva.smartgraph {
+	exports com.brunomnsilva.smartgraph.containers;
+	exports com.brunomnsilva.smartgraph.example;
+	exports com.brunomnsilva.smartgraph;
+	exports com.brunomnsilva.smartgraph.graph;
+	exports com.brunomnsilva.smartgraph.graphview;
+
+	requires java.logging;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+}
