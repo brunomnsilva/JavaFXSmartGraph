6737a0077d2292845d687eb274c2ae2a1a1a5237
Brad Hards <bradh@frogmouth.net>
2024-01-06 20:47:57 +1100

    add Java module configuration

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.brunomnsilva.smartgraph {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires java.logging;
+    
+    exports com.brunomnsilva.smartgraph;
+    exports com.brunomnsilva.smartgraph.containers;
+    exports com.brunomnsilva.smartgraph.graph;
+    exports com.brunomnsilva.smartgraph.graphview;
+}
