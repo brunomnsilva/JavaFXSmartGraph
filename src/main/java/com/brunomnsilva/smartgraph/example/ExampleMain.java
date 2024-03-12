18bc0224083ecc53eef0e5aa3d5e603b18ec250a
brunomnsilva <brunomnsilva@gmail.com>
2024-03-12 15:51:58 +0000

    Update copyright

--- src/main/java/com/brunomnsilva/smartgraph/example/ExampleMain.java
+++ src/main/java/com/brunomnsilva/smartgraph/example/ExampleMain.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * JavaFXSmartGraph | Copyright 2023  brunomnsilva@gmail.com
+ * JavaFXSmartGraph | Copyright 2023-2024  brunomnsilva@gmail.com
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
