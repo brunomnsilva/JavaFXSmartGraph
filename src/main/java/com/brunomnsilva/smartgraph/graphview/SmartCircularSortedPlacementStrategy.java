26363d8f8a8e01dc435b1e99100d90f942bed310
brunomnsilva <brunomnsilva@gmail.com>
2024-04-06 12:08:26 +0100

    Refactor and increase padding.

--- src/main/java/com/brunomnsilva/smartgraph/graphview/SmartCircularSortedPlacementStrategy.java
+++ src/main/java/com/brunomnsilva/smartgraph/graphview/SmartCircularSortedPlacementStrategy.java
@@ -38,17 +38,12 @@ import java.util.List;
  */
 public class SmartCircularSortedPlacementStrategy implements SmartPlacementStrategy {
 
+    private static final int RADIUS_PADDING = 4;
+
     @Override
     public <V, E> void place(double width, double height, SmartGraphPanel<V, E> smartGraphPanel) {
-        Point2D center = new Point2D(width / 2, height / 2);
-
+        // Sort vertices by their label
         List<SmartGraphVertex<V>> vertices = new ArrayList<>(smartGraphPanel.getSmartVertices());
-        int N = vertices.size();
-        double angleIncrement = -360f / N;
-        
-        //place first vertex at north position, others in clockwise manner
-        boolean first = true;
-        Point2D p = null;
 
         vertices.sort((v1, v2) -> {
             V e1 = v1.getUnderlyingVertex().element();
@@ -56,16 +51,23 @@ public class SmartCircularSortedPlacementStrategy implements SmartPlacementStrat
             return smartGraphPanel.getVertexLabelFor(e1).compareTo(smartGraphPanel.getVertexLabelFor(e2));
         });
 
+        //place first vertex at north position, others in clockwise manner
+        Point2D center = new Point2D(width / 2, height / 2);
+        int N = vertices.size();
+        double angleIncrement = -360f / N;
+        boolean first = true;
+        Point2D p = null;
+
         for (SmartGraphVertex<V> vertex : vertices) {
             
             if (first) {
                 //verify the smallest width and height.
                 if(width > height)
                     p = new Point2D(center.getX(),
-                            center.getY() - height / 2 + vertex.getRadius() * 2);
+                            center.getY() - height / 2 + vertex.getRadius() * RADIUS_PADDING);
                 else
                     p = new Point2D(center.getX(),
-                            center.getY() - width / 2 + vertex.getRadius() * 2);
+                            center.getY() - width / 2 + vertex.getRadius() * RADIUS_PADDING);
 
                 first = false;
             } else {
